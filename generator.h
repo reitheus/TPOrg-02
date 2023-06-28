db2582a458c01c95211a6c74a3d384085806023c
reitheus <75027532+reitheus@users.noreply.github.com>
2023-06-28 18:30:18 -0300

    Initial commit

---
+++ generator.h
@@ -0,0 +1,14 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include "cpu.h"
+
+Instruction* generateRandomInstructions(int);
+Instruction* generateMultiplicationInstructions(int, int);
+Instruction* readInstructions(char*, int*);
+Instruction* generateDivisionInstructions(int num1, int num2);
+Instruction* generatePowerInstructions(int num1, int num2);
+Instruction* generateFactorInstructions(int num1);
+Instruction* generateFibonacciInstructions(int num1);
+
+#endif // !GENERATOR_H
---
+++ main.c
@@ -0,0 +1,67 @@
+#include "cpu.h"
+#include "generator.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char**argv) {
+
+    srand(time(NULL));   // Inicializacao da semente para os numeros aleatorios.
+
+    if (argc > 5 || argc < 3) {
+        printf("Numero de argumentos invalidos! Sao 3.\n");
+        printf("Linha de execucao: ./exe TIPO_INSTRUCAO [TAMANHO_RAM|ARQUIVO_DE_INSTRUCOES]\n");
+        printf("\tExemplo 1 de execucao: ./exe random 10\n");
+        printf("\tExemplo 3 de execucao: ./exe file arquivo_de_instrucoes.txt\n");
+        return 0;
+    }
+
+    int ramSize,num1,num2;
+    Machine machine;
+    Instruction *instructions;
+
+    if (strcmp(argv[1], "random") == 0) {
+        ramSize = atoi(argv[2]);
+        instructions = generateRandomInstructions(ramSize);
+    } else if (strcmp(argv[1], "file") == 0) {
+        instructions = readInstructions(argv[2], &ramSize);
+    }else if(strcmp(argv[1], "multi")==0){
+        ramSize = atoi(argv[2]);
+        num1 = atoi(argv[3]);
+        num2 = atoi(argv[4]);
+        instructions = generateMultiplicationInstructions(num1,num2);
+    }else if(strcmp(argv[1], "div")==0){
+        ramSize = atoi(argv[2]);
+        num1 = atoi(argv[3]);
+        num2 = atoi(argv[4]);
+        instructions = generateDivisionInstructions(num1,num2);
+    }else if(strcmp(argv[1], "pow")==0){
+        ramSize = atoi(argv[2]);
+        num1 = atoi(argv[3]);
+        num2 = atoi(argv[4]);
+        instructions = generatePowerInstructions(num1,num2);
+    }else if(strcmp(argv[1], "factor")==0){
+        ramSize = atoi(argv[2]);
+        num1 = atoi(argv[3]);
+       // num2 = atoi(argv[4]);
+        instructions = generateFactorInstructions(num1);
+    }else if(strcmp(argv[1], "fib")==0){
+        ramSize = atoi(argv[2]);
+        num1 = atoi(argv[3]);
+       // num2 = atoi(argv[4]);
+        instructions = generateFibonacciInstructions(num1);
+    }else{
+        printf("Opcao invalida.\n");
+        return 0;
+    }
+    
+    printf("Iniciando a maquina...\n");
+    start(&machine, instructions, ramSize);
+    printRAM(&machine);
+    run(&machine);
+    printRAM(&machine);
+    stop(&machine);
+    printf("Finalizando a maquina...\n");
+    return 0;
+}
